c58c12fa5f7f9c580c4c71a4e01791bd112bffc3
wiroq <ward.iroq@gmail.com>
2025-05-23 21:44:31 +0300

    Prediction Phase: added support for TOF in the KNN

--- src/main/localisationMng/predictionPhase.h
+++ src/main/localisationMng/predictionPhase.h
@@ -21,13 +21,29 @@
     * @param input: sample array of RSSI values
     * @return: predicted location
 */
-LOCATIONS knnPredict(const double input[NUMBER_OF_ANCHORS]);
+LOCATIONS rssiPredict(double input[NUMBER_OF_ANCHORS]);
+
+/*
+    * @brief Predicts the location using the kNN algorithm based on input TOF values.
+    * @param input: sample array of TOF values
+    * @return: predicted location
+*/
+LOCATIONS tofPredict(double input[NUMBER_OF_RESPONDERS]);
 
 /*
     * @brief Prepares the RSSI data for prediction.
     * @param RSSIs: Array of RSSI values to be prepared.
 */
 void preparePoint(double RSSIs[NUMBER_OF_ANCHORS]);
+
+/*
+    * @brief Determines the relevance of the backup dataset.
+    *        Evaluates the dataset based on prediction accuracy.
+    *        only called at setup() assumes that dataset is already filled
+    *        with the backup dataset.
+    * @return: True if the backup dataset meets the relevance criteria, false otherwise.
+*/
+bool isBackupDataSetRelevant(void);
 
 LOCATIONS validateScanningPhasePerLabel(std::vector<Data> scanResultSamepleForValidationPerLabel);
 
